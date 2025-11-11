9e94d558cd665b44e0cdc0c645a308a7802e5065
kelislaurimae <kellulau@gmail.com>
2025-11-11 16:23:42 +0200

    lisasin questions.py faili

---
+++ kysimused.py
@@ -0,0 +1,83 @@
+kysimused = [
+    {
+        "termin": "Kärg",
+        "valikud": [
+            "Tudengite või töötajate jagatud tööruum",
+            "Arvutivõrgu sõlm, mis ühendab servereid",
+            "Üliõpilaste ühiselamu tuba"
+        ],
+        "vastus": 0
+    },
+    {
+        "termin": "Git",
+        "valikud": [
+            "Versioonihaldussüsteem, mida kasutatakse koodi muudatuste jälgimiseks ja projektides koostööks (nt GitHub, GitLab)",
+            "Failiformaat andmevahetuseks eri programmide vahel",
+            "Tehisintellekti raamatukogu masinõppeks"
+        ],
+        "vastus": 0
+    },
+    {
+        "termin": "Debug",
+        "valikud": [
+            "Vigade otsimine ja parandamine programmis",
+            "Koodi kompileerimine kiiremini töötavaks versiooniks",
+            "Programmi testimine graafilise liidese kaudu"
+        ],
+        "vastus": 0
+    },
+    {
+        "termin": "Sess",
+        "valikud": [
+            "Eksamissessioon ehk periood, mil toimuvad semestri lõpueksamid ja arvestused",
+            "Ülikooli pidu enne eksamiperioodi",
+            "Kursuse vahekokkuvõtte esitamine Moodle’is"
+        ],
+        "vastus": 0
+    },
+    {
+        "termin": "EAP",
+        "valikud": [
+            "Euroopa ainepunkt, mõõdab õppetöö mahtu (1 EAP ≈ 26 tundi tudengi tööd)",
+            "Ülikooli hinnang tudengi eksamitulemustele",
+            "Elektrooniline arvestusprotokoll õppeinfosüsteemis"
+        ],
+        "vastus": 0
+    },
+    {
+        "termin": "Progemine",
+        "valikud": [
+            "Programmeerimise lühend",
+            "Arvutimängude loomine ja testimine",
+            "Andmebaasi haldamine ja optimeerimine"
+        ],
+        "vastus": 0
+    },
+    {
+        "termin": "Push",
+        "valikud": [
+            "Gitis muudatuste üleslaadimine kaugserverisse",
+            "Koodi eemaldamine GitHubist",
+            "Muudatuste kinnitamine lokaalselt ilma salvestamata"
+        ],
+        "vastus": 0
+    },
+    {
+        "termin": "Pull",
+        "valikud": [
+            "Gitis muudatuste alla laadimine kaugserverist oma arvutisse",
+            "Koodi kustutamine lokaalsest repositoryst",
+            "Failide ühendamine erinevate projektide vahel"
+        ],
+        "vastus": 0
+    },
+    {
+        "termin": "Praks",
+        "valikud": [
+            "Praktikumi lühend",
+            "Õppeaine eksamivorm",
+            "Praktikakoht ettevõttes"
+        ],
+        "vastus": 0
+    }
+]
